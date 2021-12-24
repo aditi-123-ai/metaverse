1024566fb8cece9855afd0f3bca83b1b7d67410a
aditi-123-ai <aditijayy@gmail.com>
2021-12-25 00:50:22 +0530

    chat func complete

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import Head from 'next/head'
 import Login from '../components/Login'
 import { useMoralis } from 'react-moralis';
+import Header from '../components/Header';
+import Messages from '../components/Messages';
 
 export default function Home() {
 
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated} = useMoralis();
 
   if(!isAuthenticated) return <Login />
 
   return (
-    <div className='h-screen' >
+    <div className='h-screen overflow-y-scroll bg-gradient-to-b from-slate-800 to-fuchsia-700 overflow-hidden'>
       <Head>
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Welcome to the app</h1>
-      <button onClick={logout}>logout</button>
+      
+        <div className='max-w-screen-2xl mx-auto '>
+          {/* header */}
+          <Header/>
+          {/* messages */}
+          <Messages/>
+        </div>
+      
     </div>
   )
 }
